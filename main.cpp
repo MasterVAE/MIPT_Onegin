14d55c01177f20efa91e57204907c9162ab70597
MasterVAE <voilov07@gmail.com>
2025-09-21 12:43:44 +0300

    data to structure and initializing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 #include "code/sorting.h"
 #include "code/array.h"
 
+typedef struct Data{
+    char *buffer;
+    size_t size;
+    Line* text;
+    size_t count;
+} Data;
 
 int main()
 {
@@ -13,35 +19,30 @@ int main()
 
     printf("Starting...\n");
 
-    // FIXME struct + init
-    char *buffer;
-    size_t size;
-    Line* text;
-    size_t count;
-
+    Data data = {NULL, 0, NULL, 0};
+    
     printf("Initializing...\n");
 
     FILE *input_file = fopen(input_file_name, "r");
     assert(input_file != NULL);
-    size = file_len(input_file);
+    data.size = file_len(input_file);
 
-    initialize_buffer(&buffer, &size, input_file);
+    initialize_buffer(&data.buffer, &data.size, input_file);
     fclose(input_file);
-    count = initialize_text(&text, buffer, size);
+    data.count = initialize_text(&data.text, data.buffer, data.size);
 
     printf("Sorting...\n");
     // вставляем str_cmp или str_rcmp
-    sort((void*)text, count, sizeof(Line), str_rcmp);
+    sort((void*)data.text, data.count, sizeof(Line), str_rcmp);
 
     printf("Printing...\n");
     FILE *output_file = fopen(output_file_name, "w");
-    print(text, count, output_file);
+    print(data.text, data.count, output_file);
     fclose(output_file);
 
     printf("Freeing...\n");
-    free(text);
-    free(buffer);
-
+    free(data.text);
+    free(data.buffer);
     printf("Ended...\n");
 
     return 0;
